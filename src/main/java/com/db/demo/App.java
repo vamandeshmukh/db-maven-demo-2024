7c14ad3cf3162b91de8873f9fa80bfefec0a0302
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-23 13:07:30 +0530

    maven demo

---
+++ src/main/java/com/db/demo/App.java
@@ -0,0 +1,15 @@
+package com.db.demo;
+
+/**
+ * @author Vaman
+ * @since 2024
+ * @version 1.0.0
+ */
+
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+	}
+}
