e1f900884d323ccbcaa2e6e4fa54d43b11616c9a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-23 17:59:51 +0530

    fundas

--- src/main/java/com/db/demo/solid/s5/DipDemo.java
+++ src/main/java/com/db/demo/solid/s5/DipDemo.java
@@ -1,6 +1,5 @@
 package com.db.demo.solid.s5;
 
-
 //Dependency Inversion Principle (DIP)
 //High-level modules should not depend on low-level modules. Both should depend on abstractions.
 //Use abstractions (interfaces or abstract classes) to decouple components.
@@ -8,8 +7,17 @@ package com.db.demo.solid.s5;
 
 public class DipDemo {
 
-}
+	public static void main(String[] args) {
 
+		Printable printer1 = new Printer();
+		Printable printer2 = new Printer2();
+
+		Computer obj = new Computer(printer2);
+		obj.printDocument("abc");
+
+	}
+
+}
 
 //class Printer {
 //    public void print(String document) {
@@ -31,64 +39,43 @@ public class DipDemo {
 //    }
 //}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-interface PrinterInterface {
-    void print(String document);
+interface Printable {
+	void print(String document);
 }
 
-class Printer implements PrinterInterface {
+class Printer implements Printable {
 
 	public void print(String document) {
-
-   }
+		System.out.println("Printing with printer 1 " + document);
+	}
 }
 
-class Printer2 implements PrinterInterface {
+class Printer2 implements Printable {
 
 	public void print(String document) {
-
-   }
+		System.out.println("Printing with printer 2 " + document);
+	}
 }
-
 
 class Computer {
-    
-	private PrinterInterface printer;
 
-    public Computer(PrinterInterface printer) {
-        this.printer = printer;
-    }
+	private Printable printer;
 
-    public void printDocument(String document) {
-        printer.print(document);
-    }
+	public Computer(Printable printer) {
+		this.printer = printer;
+	}
+
+	public void printDocument(String document) {
+		printer.print(document);
+	}
 }
 
-
 class Implement {
-	
+
 	public static void main(String[] args) {
-		
+
 		Computer obj = new Computer(null);
-		
+
 	}
-	
-	
-	
+
 }
