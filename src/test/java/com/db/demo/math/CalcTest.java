4738bed5d3d7f0894f07db441d15e51cccccd87f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-24 12:00:03 +0530

    desp

--- src/test/java/com/db/demo/math/CalcTest.java
+++ src/test/java/com/db/demo/math/CalcTest.java
@@ -1,5 +1,22 @@
 package com.db.demo.math;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class CalcTest {
 
+	@Test
+	public void testTest() {
+		int expected = 10;
+		int actual = 5 + 5;
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testTest2() {
+		int expected = 11;
+		int actual = 5 + 5;
+		assertEquals(expected, actual);
+	}
 }
