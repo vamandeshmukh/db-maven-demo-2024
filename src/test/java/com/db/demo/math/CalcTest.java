41d70896be1bef630e1fb38345255351d49adbd8
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-24 12:09:35 +0530

    desp

--- src/test/java/com/db/demo/math/CalcTest.java
+++ src/test/java/com/db/demo/math/CalcTest.java
@@ -1,22 +1,50 @@
 package com.db.demo.math;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class CalcTest {
 
 	@Test
-	public void testTest() {
+	public void testTestPositive() {
 		int expected = 10;
 		int actual = 5 + 5;
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void testTest2() {
-		int expected = 11;
+	public void testTestNegative() {
+		int unexpected = 11;
 		int actual = 5 + 5;
-		assertEquals(expected, actual);
+		assertNotEquals(unexpected, actual);
 	}
 }
+
+
+
+//package com.db.demo.math;
+//
+////import static org.junit.jupiter.api.Assertions.assertEquals;
+//import org.junit.jupiter.api.Assertions;
+//
+//import org.junit.jupiter.api.Test;
+//
+//public class CalcTest {
+//
+//	@Test
+//	public void testTest() {
+//		int expected = 10;
+//		int actual = 5 + 5;
+////		assertEquals(expected, actual);
+//		Assertions.assertEquals(expected, actual);
+//	}
+//
+////	@Test
+////	public void testTest2() {
+////		int expected = 11;
+////		int actual = 5 + 5;
+////		assertEquals(expected, actual);
+////	}
+//}
