24690b419dee59450ef6e9cd0ecb98012fe3c794
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-24 12:14:58 +0530

    desp

--- src/test/java/com/db/demo/math/CalcTest.java
+++ src/test/java/com/db/demo/math/CalcTest.java
@@ -6,21 +6,48 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalcTest {
+	
+	
+	
 
 	@Test
-	public void testTestPositive() {
-		int expected = 10;
-		int actual = 5 + 5;
-		assertEquals(expected, actual);
+	public void testAddNums() {
+		Calc calc = new Calc();
+		assertEquals(10, calc.addNums(5, 5));
+	}
+	
+	@Test
+	public void testAddNums2() {
+		Calc calc = new Calc();
+		assertNotEquals(10, calc.addNums(5, 6));
 	}
 
-	@Test
-	public void testTestNegative() {
-		int unexpected = 11;
-		int actual = 5 + 5;
-		assertNotEquals(unexpected, actual);
-	}
 }
+
+
+//package com.db.demo.math;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotEquals;
+//
+//import org.junit.jupiter.api.Test;
+//
+//public class CalcTest {
+//
+//	@Test
+//	public void testTestPositive() {
+//		int expected = 10;
+//		int actual = 5 + 5;
+//		assertEquals(expected, actual);
+//	}
+//
+//	@Test
+//	public void testTestNegative() {
+//		int unexpected = 10; 
+//		int actual = 5 + 6;
+//		assertNotEquals(unexpected, actual);
+//	} 
+//}
 
 
 
