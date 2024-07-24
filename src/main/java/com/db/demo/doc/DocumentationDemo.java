cebc6d3c1a72c3ac17fe3b8bd0908bec58de4228
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-24 10:57:33 +0530

    desp

--- src/main/java/com/db/demo/doc/DocumentationDemo.java
+++ src/main/java/com/db/demo/doc/DocumentationDemo.java
@@ -1,11 +1,44 @@
 package com.db.demo.doc;
 
+/**
+ * 
+ * @author Vaman 
+ * @version 1.0.0 
+ * @since 2024 
+ * 
+ * This class demonstrates the use of Java documentation.  
+ */
+
 public class DocumentationDemo {
-	
-	
-	
-	
-	
-	
+
+  /**
+  * The main method to test the Maths class methods.
+  *
+  * @param args command line arguments
+  */
+ public static void main(String[] args) {
+     Maths maths = new Maths();
+
+     int sum = maths.addNums(10, 5);
+     System.out.println("Sum: " + sum); // Output: Sum: 15
+
+     int difference = maths.subNums(10, 5);
+     System.out.println("Difference: " + difference); // Output: Difference: 5
+ }
 
 }
+
+//class Delete {
+//
+////	single line comment 
+//
+//	/*
+//	 * these are 
+//	 * multi lines comments
+//	 */
+//	
+//	/** 
+//	 * java doc comments 
+//	 */
+//
+//}
